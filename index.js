d16a891fdf3419ee779eab7efb872151746f3730
katopz <katopz@gmail.com>
2026-09-05 21:20:34 +0700

    style: prettier format index.js and script.js

--- index.js
+++ index.js
@@ -1,177 +1,183 @@
-window.isDebug = window.location.hash === '#debug'
+window.isDebug = window.location.hash === "#debug";
 const println = (...arguments) => {
-  window.isDebug && console.log(...arguments)
-}
+  window.isDebug && console.log(...arguments);
+};
 
-var s = document.createElement('script')
-s.src = chrome.runtime.getURL('script.js')
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("script.js");
 s.onload = function () {
-  this.remove()
-}
-;(document.head || document.documentElement).appendChild(s)
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
 
-var clickId
+var clickId;
 const confirm = () => {
   // Check for yes existing button
-  const button = Array.from(document.querySelectorAll('#button')).find((e) => e.attributes['aria-label'] && e.attributes['aria-label'].nodeValue.toUpperCase() === 'YES')
+  const button = Array.from(document.querySelectorAll("#button")).find(
+    (e) =>
+      e.attributes["aria-label"] &&
+      e.attributes["aria-label"].nodeValue.toUpperCase() === "YES",
+  );
   if (button) {
     // Lazy click later prevent rapid call
-    clearTimeout(clickId)
+    clearTimeout(clickId);
     clickId = setTimeout(() => {
-      button.click()
-    }, 100)
+      button.click();
+    }, 100);
   }
-}
+};
 
 // Select the node that will be observed for mutations
-const targetNode = document.body
+const targetNode = document.body;
 
 // Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true }
+const config = { attributes: true, childList: true, subtree: true };
 
 // Callback function to execute when mutations are observed
 const callback = function (mutationsList, _observer) {
-  let isDirty = false
+  let isDirty = false;
   for (let mutation of mutationsList) {
-    if (mutation.type === 'childList' || mutation.type === 'attributes') {
-      isDirty = true
+    if (mutation.type === "childList" || mutation.type === "attributes") {
+      isDirty = true;
     }
   }
 
-  isDirty && confirm()
-}
+  isDirty && confirm();
+};
 
 // Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback)
+const observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
-observer.observe(targetNode, config)
+observer.observe(targetNode, config);
 
 // Is active
-let moveId
-let isActive = false
+let moveId;
+let isActive = false;
 const notMove = () => {
-  isActive = false
-}
+  isActive = false;
+};
 
-let isPIP = false
+let isPIP = false;
 
 const active = () => {
-  isActive = true
+  isActive = true;
 
-  clearTimeout(moveId)
-  moveId = setTimeout(notMove, 1000)
-}
+  clearTimeout(moveId);
+  moveId = setTimeout(notMove, 1000);
+};
 
-document.body.addEventListener('mousemove', active)
-document.body.addEventListener('mouseover', active)
-document.body.addEventListener('mousedown', active)
-document.body.addEventListener('keydown', active)
-document.body.addEventListener('scroll', active)
-document.body.addEventListener('touchstart', active)
+document.body.addEventListener("mousemove", active);
+document.body.addEventListener("mouseover", active);
+document.body.addEventListener("mousedown", active);
+document.body.addEventListener("keydown", active);
+document.body.addEventListener("scroll", active);
+document.body.addEventListener("touchstart", active);
 
-document.addEventListener('visibilitychange', function () {
-  println('visibilitychange...', document.visibilityState)
-  if (document.visibilityState === 'visible') {
-    confirm()
+document.addEventListener("visibilitychange", function () {
+  println("visibilitychange...", document.visibilityState);
+  if (document.visibilityState === "visible") {
+    confirm();
   } else {
     // do something when not visible
   }
-})
+});
 
 const onPause = (e) => {
-  println('pause...:', isActive, isPIP)
+  println("pause...:", isActive, isPIP);
   // Hide anyway
-  confirm()
+  confirm();
 
   // Backdrop?
-  const isBackDrop = document.querySelector('body > tp-yt-iron-overlay-backdrop')
+  const isBackDrop = document.querySelector(
+    "body > tp-yt-iron-overlay-backdrop",
+  );
   if (isBackDrop) {
     // Play anyway
-    e.target && e.target.play()
-    return
+    e.target && e.target.play();
+    return;
   }
 
   // Active?
-  if (isActive) return
+  if (isActive) return;
 
   // plan B
-  println('play!!')
-  e.target && e.target.play()
+  println("play!!");
+  e.target && e.target.play();
 
   // plan C
-  const video = document.querySelector('video')
+  const video = document.querySelector("video");
   if (video) {
-    video.play()
+    video.play();
   }
-}
+};
 
 const onEnterPip = (e) => {
-  println('enter pip...')
-  isPIP = true
-}
+  println("enter pip...");
+  isPIP = true;
+};
 
 const onLeavePip = (e) => {
-  println('leave pip...')
-  isPIP = false
-  confirm()
-}
+  println("leave pip...");
+  isPIP = false;
+  confirm();
+};
 
 const watch = () => {
-  println('watch...')
+  println("watch...");
 
   // Alway play
-  const video = document.querySelector('video')
-  video.removeEventListener('pause', onPause)
-  video.addEventListener('pause', onPause)
+  const video = document.querySelector("video");
+  video.removeEventListener("pause", onPause);
+  video.addEventListener("pause", onPause);
 
   // PIP
-  video.addEventListener('enterpictureinpicture', onEnterPip, false)
-  video.addEventListener('leavepictureinpicture', onLeavePip, false)
+  video.addEventListener("enterpictureinpicture", onEnterPip, false);
+  video.addEventListener("leavepictureinpicture", onLeavePip, false);
 
   // Watch for human pause
-  moveId = setTimeout(notMove, 1000)
-}
+  moveId = setTimeout(notMove, 1000);
+};
 
 const initVideo = () => {
-  println('initVideo...')
+  println("initVideo...");
 
   // On homepage?
-  if (window.location.pathname !== '/watch') {
-    setTimeout(initVideo, 1000)
-    return
+  if (window.location.pathname !== "/watch") {
+    setTimeout(initVideo, 1000);
+    return;
   }
 
   // Video ready?
-  const video = document.querySelector('video')
+  const video = document.querySelector("video");
   if (!video) {
-    setTimeout(initVideo, 1000)
-    return
+    setTimeout(initVideo, 1000);
+    return;
   }
 
-  watch()
-}
+  watch();
+};
 
 // Watch for dom change
-let oldHref = document.location.href
+let oldHref = document.location.href;
 
 window.onload = function () {
-  const bodyList = document.querySelector('body')
+  const bodyList = document.querySelector("body");
 
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (_mutation) {
       if (oldHref != document.location.href) {
-        oldHref = document.location.href
-        initVideo()
+        oldHref = document.location.href;
+        initVideo();
       }
-    })
-  })
+    });
+  });
 
   const config = {
     childList: true,
-    subtree: true
-  }
+    subtree: true,
+  };
 
-  observer.observe(bodyList, config)
-  initVideo()
-}
+  observer.observe(bodyList, config);
+  initVideo();
+};
--- script.js
+++ script.js
@@ -1,81 +1,95 @@
-window.isDebug = window.location.hash === '#debug'
+window.isDebug = window.location.hash === "#debug";
 const println = (...arguments) => {
-  window.isDebug && console.log(...arguments)
-}
+  window.isDebug && console.log(...arguments);
+};
 
 // https://gist.github.com/adisib/1e6b429b9bb630fceb170f3fa77c57a3
 const alwaysHD = () => {
-  println('alwaysHD...')
+  println("alwaysHD...");
   // On homepage?
-  if (window.location.pathname !== '/watch') {
-    setTimeout(alwaysHD, 1000)
-    return
+  if (window.location.pathname !== "/watch") {
+    setTimeout(alwaysHD, 1000);
+    return;
   }
 
-  println('ytPlayer...')
-  const ytPlayer = document.getElementById('movie_player') || document.getElementsByClassName('html5-video-player')[0]
+  println("ytPlayer...");
+  const ytPlayer =
+    document.getElementById("movie_player") ||
+    document.getElementsByClassName("html5-video-player")[0];
   if (!ytPlayer) {
-    setTimeout(alwaysHD, 1000)
-    return
+    setTimeout(alwaysHD, 1000);
+    return;
   }
 
-  const current_quality = ytPlayer.getPlaybackQuality ? ytPlayer.getPlaybackQuality() : ''
-  const expected_quality = ytPlayer.getAvailableQualityLevels()[0]
+  const current_quality = ytPlayer.getPlaybackQuality
+    ? ytPlayer.getPlaybackQuality()
+    : "";
+  const expected_quality = ytPlayer.getAvailableQualityLevels()[0];
 
-  println('current_quality:', current_quality)
-  println('expected_quality:', expected_quality)
+  println("current_quality:", current_quality);
+  println("expected_quality:", expected_quality);
 
   // Already HD
   if (current_quality === expected_quality) {
-    println('all set:', expected_quality)
-    return
+    println("all set:", expected_quality);
+    return;
   }
 
   // Can be set?
   if (!ytPlayer.stopVideo) {
-    println('no ytPlayer.stopVideo')
-    return
+    println("no ytPlayer.stopVideo");
+    return;
   }
 
   // Set to HD
-  println('setPlaybackQuality:', expected_quality)
-  ytPlayer.stopVideo()
-  ytPlayer.setPlaybackQualityRange && ytPlayer.setPlaybackQualityRange(expected_quality)
-  ytPlayer.setPlaybackQuality(expected_quality)
-  let storedQuality = localStorage.getItem('yt-player-quality')
+  println("setPlaybackQuality:", expected_quality);
+  ytPlayer.stopVideo();
+  ytPlayer.setPlaybackQualityRange &&
+    ytPlayer.setPlaybackQualityRange(expected_quality);
+  ytPlayer.setPlaybackQuality(expected_quality);
+  let storedQuality = localStorage.getItem("yt-player-quality");
   if (!storedQuality || storedQuality.indexOf(expected_quality) === -1) {
     let tc = Date.now(),
-      te = tc + 2592000000
-    localStorage.setItem('yt-player-quality', '{"data":"' + targetRes + '","expiration":' + te + ',"creation":' + tc + '}')
+      te = tc + 2592000000;
+    localStorage.setItem(
+      "yt-player-quality",
+      '{"data":"' +
+        targetRes +
+        '","expiration":' +
+        te +
+        ',"creation":' +
+        tc +
+        "}",
+    );
   }
 
-  ytPlayer.playVideo()
-}
+  ytPlayer.playVideo();
+};
 
 // Watch for dom change
-let oldHref = document.location.href
+let oldHref = document.location.href;
 
 window.onload = function () {
-  const bodyList = document.querySelector('body')
+  const bodyList = document.querySelector("body");
 
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (_mutation) {
       if (oldHref != document.location.href) {
-        oldHref = document.location.href
-        alwaysHD()
+        oldHref = document.location.href;
+        alwaysHD();
       }
-    })
-  })
+    });
+  });
 
   const config = {
     childList: true,
-    subtree: true
-  }
+    subtree: true,
+  };
 
-  observer.observe(bodyList, config)
-  alwaysHD()
+  observer.observe(bodyList, config);
+  alwaysHD();
 
   // Alway HD
-  const video = document.querySelector('video')
-  video.addEventListener('play', alwaysHD)
-}
+  const video = document.querySelector("video");
+  video.addEventListener("play", alwaysHD);
+};
